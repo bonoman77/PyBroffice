24b769e3ce105c2daa422671ff661f0ff6ef5801
SeungKyun <bonoman@me.com>
2026-02-04 08:34:00 +0900

    테스트환경 주석 추가

--- start_broffice.py
+++ start_broffice.py
@@ -1,6 +1,11 @@
 # import os 
 # os.environ['FLASK_ENV'] = 'production'
 
+# 원격 테스트 환경 (SSH 터널 사용)
+# 사전에 SSH 터널을 열어야 함:
+# ssh -L 3307:Broffice.mysql.pythonanywhere-services.com:3306 Broffice@ssh.pythonanywhere.com -N
+
+
 # from절을 호출하는 순간 해당 프로젝트의 __init__은 자동실행
 from broffice import create_app
 
